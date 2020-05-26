223180a50c9c095e6e3e125b5377c11f4448ec66
손재이 <jae2son@gmail.com>
2020-05-26 19:13:50 +0900

    yahoo!!!!!

---
+++ Project526/exam526.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	//exam1();
+	//exam2();
+	exam3();
+}
+
+int exam1() {
+	int m;
+	int n;
+	int sum = 0;
+	int temp;
+
+	printf("양수를 2개 입력 : ");
+	scanf("%d%d", &m, &n);
+
+	if (m > n) {
+		temp = m;
+		m = n;
+		n = temp;
+	}
+
+	for (; m <= n; m++) {
+		sum += m;
+	}
+
+	printf("합 : %d", sum);
+
+	return 0;
+}
+
+int exam2() {
+	int sum = 0;
+	
+	for (int i = 0; i <= 100; i++) {
+		if (i % 2 == 1) {
+			sum += i;
+		}
+		else {
+			sum -= i;
+		}
+	}
+
+	printf("연산 결과 : %d", sum);
+
+	return 0;
+}
+
+int exam3() {
+	int n;
+	int result = 1;
+
+	printf("양수 한 개 입력 : ");
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		result *= 2;
+	}
+
+	printf("출력 : %d", result);
+
+	return 0;
+}
+
+int exam4() {
+	printf("Hello world!");
+}
